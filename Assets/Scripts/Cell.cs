15eae919a0923c827f654edef865f35d3df681a5
Awasaky <awasaky@gmail.com>
2020-01-27 17:15:44 +0500

    Initial copy this piece of shit

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+  public bool controlsNord, controlsEast, controlsSouth, controlsWest;
+
+  struct Controls
+  {
+    public static bool Nord, East, South, West;
+  }
+
+  struct Connectors
+  {
+    public static bool Nord, East, South, West;
+  }
+
+  struct Links
+  {
+    public static Cell Nord, East, South, West;
+  }
+
+  struct Connections
+  {
+    public static bool Nord, East, South, West;
+  }
+
+  void Start()
+  {
+    Controls.Nord = controlsNord;
+    Controls.East = controlsEast;
+    Controls.South = controlsSouth;
+    Controls.West = controlsWest;
+
+    Connectors.Nord = Controls.Nord;
+    Connectors.East = Controls.East;
+    Connectors.South = Controls.South;
+    Connectors.West = Controls.West;
+  }
+
+  void Update()
+  {
+
+  }
+}
---
+++ Assets/Scripts/PlayFieldBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayFieldBehaviour : MonoBehaviour
+{
+  public GameObject cellType0;
+  public GameObject cellType1;
+  public GameObject cellType2;
+  public GameObject cellType3;
+  public GameObject cellType4;
+  GameObject[,] FieldMap;
+
+  void Start()
+  {
+    FieldMap = new GameObject[4, 5];
+    //initial codes of cells
+    int[,] FieldCode = new int[4, 5] {
+      { 2, 1, 3, 1, 2 },
+      { 2, 1, 3, 1, 2 },
+      { 2, 1, 3, 1, 2 },
+      { 2, 1, 3, 1, 2 }
+    };
+  }
+
+  void Update()
+  {
+
+  }
+
+}
